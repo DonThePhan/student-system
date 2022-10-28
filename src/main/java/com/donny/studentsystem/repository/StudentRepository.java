bc7a1360c84b26c34d986f89d06eef5a5803fd49
donthephan <donthephan@gmail.com>
2022-10-28 15:41:57 -0400

    repository model complete

--- src/main/java/com/donny/studentsystem/repository/StudentRepository.java
+++ src/main/java/com/donny/studentsystem/repository/StudentRepository.java
@@ -1,4 +1,9 @@
 package com.donny.studentsystem.repository;
 
-public interface StudentRepository {
+import com.donny.studentsystem.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student,Integer> {
 }
